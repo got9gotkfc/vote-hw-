781fa8021ac6a3a9c66c24ce8db793ede780d4d2
Minilin <a0966678326@gmail.com>
2022-06-21 20:20:50 +0800

    補上註釋

--- vote/vote.php
+++ vote/vote.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>投票中心</title>
 </head>
+
 <body>
-<div id="header">
-    <h1>投票中心</h1>
+    <div id="header">
+        <h1>投票中心</h1>
         <nav>
             <a href="../vote/vote.php">Votes</a>
             <a href="../index.php">Home</a>
@@ -26,38 +28,43 @@
             ?>
         </nav>
     </div>
-    <form action="./api/add_vote.php" method="post">
-    <div >
-        <label for="subject">投票主題</label>
-        <input type="text" name="subject" id="subject">
-        <input type="button" value="新增選項" onclick="more()">
-    </div>
-    <div id="selector">
-        <input type="radio" name="multiple" value="0" checked>
-        <label>單選</label>
-        <input type="radio" name="multiple" value="1" >
-        <label>複選</label>
-    </div>
-    <div id="options">
+    <form action="add_vote.php" method="post">
         <div>
-            <!--當輸入的項目為多項時,記得name的屬性要以array option[]-->
-            <label>選項:</label><input type="text" name="option[]">
+            <label for="subject">投票主題</label>
+            <input type="text" name="subject" id="subject">
+            <input type="button" value="新增選項" onclick="more()">
         </div>
-    </div>
-    <input type="submit" value="新增">
+        <div id="selector">
+            <!-- checked為預設選項 -->
+            <input type="radio" name="multiple" value="0" checked>
+            <label>單選</label>
+            <input type="radio" name="multiple" value="1">
+            <label>複選</label>
+        </div>
+        <div id="options">
+            <div>
+                <!--當輸入的項目為多項時,name的屬性要以array option[]-->
+                <label>選項:</label><input type="text" name="option[]">
+            </div>
+        </div>
+        <input type="submit" value="新增">
 
     </form>
     <script>
-function more(){
-    let opt=`<div id="new"><label>選項:</label><input type="text" name="option[]"><input type="button" value="刪除" onclick="del()"></div>`;
-    let opts=document.getElementById('options').innerHTML;
-    opts=opts+opt;
-    document.getElementById('options').innerHTML=opts;
-}
-function del(){
-    let opts=document.getElementById('new');
-     opts.remove('new');
-}
-</script>
+        // 新增選項
+        function more() {
+                                                                                            // 補上刪除按鈕
+            let opt = `<div id="new"><label>選項:</label><input type="text" name="option[]"><input type="button" value="刪除" onclick="del()"></div>`;
+            let opts = document.getElementById('options').innerHTML;
+            opts = opts + opt;
+            document.getElementById('options').innerHTML = opts;
+        }
+        // 刪除id為new的元素
+        function del() {
+            let opts = document.getElementById('new');
+            opts.remove('new');
+        }
+    </script>
 </body>
+
 </html>
